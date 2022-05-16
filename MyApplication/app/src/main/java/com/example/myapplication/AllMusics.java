6834c3e4a3c749905e0867f0144f7a592225abfb
mamamia140o <m-h-k140@hotmail.com>
2022-05-16 13:21:49 +0300

    finished music app

--- MyApplication/app/src/main/java/com/example/myapplication/AllMusics.java
+++ MyApplication/app/src/main/java/com/example/myapplication/AllMusics.java
@@ -1,158 +1,82 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
-import android.app.admin.DevicePolicyManager;
-import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Parcelable;
-import android.os.TestLooperManager;
 import android.provider.MediaStore;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
-
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
 
 public class AllMusics extends AppCompatActivity {
 
-    ListView listView;
-    String[] items;
+    RecyclerView recyclerView;
+    TextView noMusicTextView;
+    ArrayList<AudioModel> songsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_musics);
 
-        listView = (ListView) findViewById(R.id.listView);
+        recyclerView = findViewById(R.id.recycler_view);
+        noMusicTextView = findViewById(R.id.no_songs_text);
 
-        runTimePermissions();
-    }
-    public void runTimePermissions(){
-        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
-            @Override
-            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                displaySongs();
-            }
-
-            @Override
-            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-
-            }
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                permissionToken.continuePermissionRequest();
-            }
-        }).check();
-    }
-
-    public ArrayList<File> findsong(File file){
-        ArrayList<File> arrayList = new ArrayList<>();
-        File[] files = file.listFiles();
-
-        for(File singleFile: files){
-            if(singleFile.isDirectory() && !singleFile.isHidden()){
-                arrayList.addAll(findsong(singleFile));
-            }
-            else{
-                if(singleFile.getName().endsWith(".mp3")){
-                    arrayList.add(singleFile);
-                }
-            }
+        if(!checkPermission()){
+            requestPermission();
+            return;
         }
-        return arrayList;
-
-    }
-    public void displaySongs(){
-        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
 
         String[] projection = {
-                MediaStore.Audio.Media._ID,
-                MediaStore.Audio.Media.ARTIST,
                 MediaStore.Audio.Media.TITLE,
                 MediaStore.Audio.Media.DATA,
-                MediaStore.Audio.Media.DISPLAY_NAME,
                 MediaStore.Audio.Media.DURATION
         };
 
-        Cursor cursor = this.managedQuery(
-                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                projection,
-                selection,
-                null, "TITLE ASC");
+        String selection = MediaStore.Audio.Media.IS_MUSIC +" != 0";
 
-        List<String> songs = new ArrayList<String>();
+        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,projection,selection,null,null);
         while(cursor.moveToNext()){
-            songs.add(cursor.getString(0) + "||" + cursor.getString(1) + "||" +   cursor.getString(2) + "||" +   cursor.getString(3) + "||" +  cursor.getString(4) + "||" +  cursor.getString(5));
+            AudioModel songData = new AudioModel(cursor.getString(1),cursor.getString(0),cursor.getString(2));
+            if(new File(songData.getPath()).exists())
+                songsList.add(songData);
         }
-        System.out.println(songs);
-        try{
-            items = new String[songs.size()];
-
-            for(int i=0; i< songs.size();i++){
-                items[i] = songs.get(i).toString();
-            }
-            customAdapter customAdapter = new customAdapter();
-            listView.setAdapter(customAdapter);
-
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view,int position,long id){
-                    String songName = (String) listView.getItemAtPosition(position);
-                    startActivity(new Intent(getApplicationContext(),MediaPlayerActivity.class)
-                            .putExtra("songs", (Parcelable) songs)
-                            .putExtra("position",position)
-                            .putExtra("songname",songName)
-                    );
-                }
-            });
-
-        }catch (Exception e){
-            System.out.println(e);
-        }
-
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MusicListAdapter(songsList,getApplicationContext()));
+        cursor.close();
     }
-    class customAdapter extends BaseAdapter{
 
-        @Override
-        public int getCount() {
-            return items.length;
+    boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(AllMusics.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(result == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            return false;
         }
+    }
 
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
+    void requestPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(AllMusics.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Toast.makeText(AllMusics.this,"READ PERMISSION IS REQUIRED,PLEASE ALLOW FROM SETTINGS",Toast.LENGTH_SHORT).show();
+        }else
+            ActivityCompat.requestPermissions(AllMusics.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},123);
+    }
 
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            View view1 = getLayoutInflater().inflate(R.layout.list_items,null);
-            TextView txtSong = view1.findViewById(R.id.txtSong);
-            txtSong.setSelected(true);
-            txtSong.setText(items[i]);
-
-            return view1;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(recyclerView!=null){
+            recyclerView.setAdapter(new MusicListAdapter(songsList,getApplicationContext()));
         }
     }
 }
