fb82c1053ff8dc03a6dd9414af953c94ce9b51c2
mamamia140o <m-h-k140@hotmail.com>
2022-05-19 22:27:39 +0300

    last commit 22.25

--- MyApplication/app/src/main/java/com/example/myapplication/MyBroadcastReceiver.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MyBroadcastReceiver.java
@@ -12,16 +12,14 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-
-
         if ("com.example.myapplication.IN_POCKET_TRIGGER".equals(intent.getAction())) {
             Toast.makeText(context,"ses arttırıldı",Toast.LENGTH_SHORT).show();
             AudioManager audio = (AudioManager) context.getSystemService(context.AUDIO_SERVICE);
             int currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
             int maxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
             float percent = 0.9f;
-            int seventyVolume = (int) (maxVolume*percent);
-            audio.setStreamVolume(AudioManager.STREAM_MUSIC, seventyVolume, 0);
+            int ninetyVolume = (int) (maxVolume*percent);
+            audio.setStreamVolume(AudioManager.STREAM_MUSIC, ninetyVolume, 0);
 
         }
         if ("com.example.myapplication.ON_TABLE_TRIGGER".equals(intent.getAction())) {
@@ -30,8 +28,8 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
             int currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
             int maxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
             float percent = 0.1f;
-            int seventyVolume = (int) (maxVolume*percent);
-            audio.setStreamVolume(AudioManager.STREAM_MUSIC, seventyVolume, 0);
+            int tenVolume = (int) (maxVolume*percent);
+            audio.setStreamVolume(AudioManager.STREAM_MUSIC, tenVolume, 0);
 
         }
 
